d5bc538799a94472c3f3b3f604fc097e02096e35
artismarti <artismarti@gmail.com>
2018-12-10 11:54:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser = 'Arti Smarti'
+let currentUser = 'Ada Lovelace'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
 let shortGreeting = 'Welcome, ' + currentUser[0] + '!'
